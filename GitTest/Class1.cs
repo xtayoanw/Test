e453d86e51a5ffce285f543f60ac0d9946c1b681
Lian Zhang <Lian_Zhang5@jabil.com>
2022-01-06 11:30:16 +0800

    TEST 2

---
+++ GitTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    public class Class1
+    {
+        static public void MSG()
+        {
+            Console.WriteLine("Hello World from Class1");
+        }
+    }
+}
