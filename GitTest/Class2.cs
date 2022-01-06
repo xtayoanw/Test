878fddd68b9a7ffd107df7d82b049ce8788e939a
Lian Zhang <Lian_Zhang5@jabil.com>
2022-01-06 14:17:01 +0800

    add class2

---
+++ GitTest/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    public class Class2
+    {
+        static public void MSG()
+        {
+            Console.WriteLine("Hello World from Class2");
+        }
+        static public void MSG2()
+        {
+            Console.WriteLine("Hello World2 from Class2");
+        }
+    }
+}
